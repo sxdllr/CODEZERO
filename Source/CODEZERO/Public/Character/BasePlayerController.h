57db0e321a56e738e75821ee35ca7ae45e8c4bf2
Yurii <yuriyperederiyyy@gmail.com>
2024-05-01 21:21:52 +0300

    Create BasePlayerController class

---
+++ Source/CODEZERO/Public/Character/BasePlayerController.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODEZERO_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
