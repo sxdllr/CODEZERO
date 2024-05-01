02dd8f6b1c77099a815def7fe1c11d4bfc9b6ec7
Yurii <yuriyperederiyyy@gmail.com>
2024-05-01 21:43:51 +0300

    Add InputMapping

--- Source/CODEZERO/Private/Character/BasePlayerController.cpp
+++ Source/CODEZERO/Private/Character/BasePlayerController.cpp
@@ -1,3 +1,16 @@
 
 #include "Character/BasePlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ABasePlayerController::ABasePlayerController()
+	: InputMappingContext(nullptr) {}
+
+void ABasePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+	}
+}
--- Source/CODEZERO/Public/Character/BasePlayerController.h
+++ Source/CODEZERO/Public/Character/BasePlayerController.h
@@ -5,6 +5,8 @@
 #include "GameFramework/PlayerController.h"
 #include "BasePlayerController.generated.h"
 
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -12,5 +14,13 @@ UCLASS()
 class CODEZERO_API ABasePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+protected:
+	ABasePlayerController();
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
 	
 };
