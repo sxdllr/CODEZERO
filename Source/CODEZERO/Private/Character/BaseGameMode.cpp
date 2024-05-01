de4541840b125e98766db6ab91b489828d2e6033
Yurii <yuriyperederiyyy@gmail.com>
2024-05-01 21:20:12 +0300

    Create BaseGameMode class

---
+++ Source/CODEZERO/Private/Character/BaseGameMode.cpp
@@ -0,0 +1,3 @@
+
+#include "Character/BaseGameMode.h"
+
---
+++ Source/CODEZERO/Public/Character/BaseGameMode.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODEZERO_API ABaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
