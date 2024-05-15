4d2656a0935958edcd6fb6c38d442eee160046c9
Yurii <yuriyperederiyyy@gmail.com>
2024-05-15 22:53:59 +0300

    Add ItemBase class

---
+++ Source/CODEZERO/Private/Inventory/ItemBase.cpp
@@ -0,0 +1,44 @@
+
+#include "Inventory/ItemBase.h"
+#include "Character/BaseCharacter.h"
+
+UItemBase::UItemBase()
+{
+}
+
+UItemBase* UItemBase::CreateItemCopy() const
+{
+	UItemBase* ItemCopy = NewObject<UItemBase>(StaticClass());
+
+	ItemCopy->ID = this->ID;
+	ItemCopy->Quantity = this->Quantity;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->ItemStats = this->ItemStats;
+	ItemCopy->TextData = this->TextData;
+	ItemCopy->NumericData = this->NumericData;
+	ItemCopy->AssetData = this->AssetData;
+
+	return ItemCopy;
+}
+
+void UItemBase::SetQuantity(const int32 NewQuantity)
+{
+	if(NewQuantity != Quantity)
+	{
+		Quantity = FMath::Clamp(NewQuantity, 0, NumericData.bIsStackable ? NumericData.MaxStackSize : 1);
+
+		/*
+		if(OwningInventory)
+		{
+			if(Quantity <= 0)
+			{
+				OwningInventory->RemoveItem(this);
+			}
+		}
+		*/
+	}
+}
+
+void UItemBase::Use(ABaseCharacter* Character)
+{
+}
---
+++ Source/CODEZERO/Public/Inventory/ItemBase.h
@@ -0,0 +1,54 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDataBase.h"
+//#include "UObject/NoExportTypes.h"
+#include "ItemBase.generated.h"
+
+class ABaseCharacter;
+
+UCLASS()
+class CODEZERO_API UItemBase : public UObject
+{
+	GENERATED_BODY()
+
+	//UPROPERTY()
+	//UInventoryComponent* OwingInventory;
+
+	UPROPERTY(VisibleAnywhere, Category = "Item Data", meta = (UIMin = 1, UIMax = 128, AllowPrivateAccess = true))
+	int32 Quantity;
+	
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	FName ID;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	EITemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	FItemStats ItemStats;
+	
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data", meta = (AllowPrivateAccess = true))
+	FItemAssetData AssetData;
+
+public:
+	UItemBase();
+
+	UItemBase* CreateItemCopy() const;
+
+	float GetItemStackSquare() const { return NumericData.ItemSquare; }
+	bool IsFullItemStack() const { return Quantity == NumericData.MaxStackSize; }
+
+	void SetQuantity(const int32 NewQuantity);
+
+	virtual void Use(ABaseCharacter* Character);
+
+protected:
+	bool operator==(const FName& OtherID) const { return ID == OtherID; }
+};
